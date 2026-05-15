d86fb63b6e596d39dedc222d214f1ca2fcf95d07
Lateefat Obisesan <temilele06@gmail.com>
2026-05-15 14:40:38 -0500

    writing the codes for the review

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,6 +1,11 @@
-function Star() {
+import {FontAwesomeIcon } from "@fontawesome/react-fontawesome";
+import {faStar} from "@fortawesome/free-brands-svg-icons";
+
+function Star(props) {
     return(
-        <></>
+        <FontAwesomeIcon icon={faStar} className={props.selected ? "satr selected" : "star"}
+        onClick={props.onClick}
+        />
     );
 }
 
