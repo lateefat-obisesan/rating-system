dbec977f245af883fd6146c6f0c6203fd9d3520e
Lateefat Obisesan <temilele06@gmail.com>
2026-05-15 15:05:12 -0500

    undating the star.js

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,7 +1,7 @@
 import {FontAwesomeIcon } from "@fontawesome/react-fontawesome";
 import {faStar} from "@fortawesome/free-brands-svg-icons";
 
-function Star(props) {
+function Star(selected, onClick) {
     return(
         <FontAwesomeIcon icon={faStar} className={props.selected ? "satr selected" : "star"}
         onClick={props.onClick}
