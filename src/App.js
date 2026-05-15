f9b25339d9c2bce7a363285789acca7d5d58c8bb
Lateefat Obisesan <temilele06@gmail.com>
2026-05-15 14:50:50 -0500

    added content to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import {useState} from "react";
+import Dialog from "./components/Dialog";
 
 
 function App() {
+  const [showDialog, setShowDialog] = useState(false);
+
   return (
-   <></>
+   <div className="App">
+    <button onClick={() => setShowDialog(true)}>
+      Open Dialog
+    </button>
+    {showDialog && (
+      <Dialog closeDialog={() => setShowDialog(false)} />
+    )}
+   </div>
   );
 }
 
--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,6 +1,15 @@
-function Dialog() {
+import StarRating from "./StarRating";
+
+function Dialog(props) {
     return(
-        <></>
+        <div className="dialog-overlay">
+            <div className="dialog-box">
+                <button className="close-btn" onClick={props.closeDialog}>
+                    X
+                </button>
+                <h2>Come back next time.</h2>
+            </div>
+        </div>
     );
 }
 
